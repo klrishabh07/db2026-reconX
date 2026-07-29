e4b971d9db4941200c8da0055b028445030242db
klrishabh07 <klrishabh07@gmail.com>
2026-07-28 21:10:30 -0700

    Upto Day 3 updated

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-28T03:09:50-0700",
+    date = "2026-07-28T20:40:49-0700",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
